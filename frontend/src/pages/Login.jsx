3853767aa452e022018ce6203126cb6b7c65baf0
odraciroconit <155962513+odraciroconit@users.noreply.github.com>
2024-12-19 16:55:35 +0000

    teste

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -63,7 +63,7 @@ export default function Login() {
             <br />
             <br />
             <div>
-                <label htmlFor="email">Email:</label>
+                <label htmlFor="email">Email123:</label>
                 <input
                     type="email"
                     id="email"
